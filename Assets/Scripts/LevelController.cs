c412e5cb3437ccf0794dee204f90a36e5cff4a2b
NezcafeGold <NezcafeGold@users.noreply.github.com>
2020-07-25 22:23:18 +0500

    edit prvs

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private Level[] levels;
+    [SerializeField] private string lvlName;
+
+    public Level[] Levels
+    {
+        get { return levels; }
+    }
+
+    public string LvlName
+    {
+        get { return lvlName; }
+        set { lvlName = value; }
+    }
+}
